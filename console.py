86e4c81b899450096679d7343ce73acf3d6b0ac5
Samson <samtob2002@gmail.com>
2024-05-20 15:33:27 +0100

    including documentation

--- console.py
+++ console.py
@@ -5,21 +5,51 @@ from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
+    """
+    A class that inherits from the cmd.Cmd class. This class is used to define
+    the commands available in the HBNB console.
+    """
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        "Quit command to exit the program"
+        """
+        Quit command to exit the program.
+
+        Args:
+            arg (str): Unused argument
+
+        Returns:
+            bool: True to exit the program
+        """
         return True
 
     def do_EOF(self, arg):
-        "Exiting using EOF"
+        """
+        Exiting using EOF.
+
+        Args:
+            arg (str): Unused argument
+
+        Returns:
+            bool: True to exit the program
+        """
         return True
 
     def do_help(self, arg):
-        "getting information on each keyword"
+        """
+        Getting information on each keyword.
+
+        Args:
+            arg (str): The keyword to get information on
+
+        Returns:
+            None
+        """
         cmd.Cmd.do_help(self, arg)
 
-
+    def emptyline(self):
+        "Do nothing on empty line"
+        pass
 
 
 if __name__ == "__main__":
